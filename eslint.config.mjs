5fec75ebad200293660546401a3168068b7b2b40
andkymon <andkymon@gmail.com>
2025-02-12 15:50:53 +0800

    feat: add env config for ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,13 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: { globals: globals.browser },
+    env: {
+      browser: true,
+      amd: true,
+      node: true,
+    },
+  },
   pluginJs.configs.recommended,
 ];
