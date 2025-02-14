875ae4ddf7de21c17952b993705de0a9bc6fca6a
andkymon <andkymon@gmail.com>
2025-02-14 19:42:19 +0800

    Initial testing of placeShip()
    - Validates ship placement
    - Places ship in desired orientation

--- src/gameboard.js
+++ src/gameboard.js
@@ -2,9 +2,11 @@ import { Ship } from "./ship";
 
 export class Gameboard {
     constructor() {
-        this.board = new Array(10).fill(new Array(10).fill(null));
+        // TODO: Make getter
+        this.board = new Array(10).fill(null).map(() => new Array(10).fill(null));
     }
 
+    // TODO: Make Private
     isValidCoordinates(coordinates) {
         for (const value of coordinates) {
             if (value > 9 || value < 0) {
@@ -14,14 +16,24 @@ export class Gameboard {
         return true;
     }
 
-    placeShip(ship, col, row, isVertical) {
-        const rowIncrement = isVertical ? 1 : 0;
-        const colIncrement = isVertical ? 0 : 1;
+    placeShip(ship, row, col, isVertical) {
+        const endRow = isVertical ? row + (ship.length - 1) : row;
+        const endCol = isVertical ? col : col + (ship.length - 1);
+
+        // To prevent placing ships at invalid coordinates, check the validity of the last cell the ship will occupy
+        if (this.isValidCoordinates([endRow, endCol]) === false) {
+            return "Out of bounds.";  
+        }
 
         for (let i = 0; i < ship.length; i++) {
             this.board[row][col] = ship;
-            row += rowIncrement;
-            col += colIncrement;
+
+            // If vertical, increment row; If horizontal, increment column
+            if (isVertical) {
+                row++;
+            } else {
+                col++;
+            }
         }
     }
 }
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,22 +1,35 @@
 import { Gameboard } from "./gameboard";
+import { Ship } from "./ship";
 
 const testGameboard = new Gameboard();
 
+
 describe("constructor()", () => {
     test("should initialize 10x10 2D array filled with null, representing the board", () => {
-        const sampleArray = new Array(10).fill(new Array(10).fill(null));
+        const sampleArray = new Array(10).fill(null).map(() => new Array(10).fill(null));
         expect(testGameboard.board).toStrictEqual(sampleArray);
     });
 });
 
-describe("isValidCoordinates()", () => {
-    test("should return true on valid coordinates", () => {
-        expect(testGameboard.isValidCoordinates([0, 2])).toBe(true);
-        expect(testGameboard.isValidCoordinates([5, 5])).toBe(true);
+describe("placeShip()", () => {
+    const testShip = new Ship(3);
+
+    test("should not allow ship placement when a ship hits an invalid coordinate", () => {
+        expect(testGameboard.placeShip(testShip, 0, 9, false)).toBe("Out of bounds.");
     });
 
-    test("should return false on valid coordinates", () => {
-        expect(testGameboard.isValidCoordinates([-1, 2])).toBe(false);
-        expect(testGameboard.isValidCoordinates([10, 10])).toBe(false);
+    test("should place ship vertically when isVertical === true", () => { 
+        testGameboard.placeShip(testShip, 1, 5, false);
+        expect(testGameboard.board[1]).toStrictEqual([null, null, null, null, null, testShip, testShip, testShip, null, null]);
     });
+    
+    test("should place ship vertically when isVertical === false", () => { 
+        const testShip2 = new Ship(3);
+        testGameboard.placeShip(testShip2, 5, 5, true);
+        expect(testGameboard.board[5]).toStrictEqual([null, null, null, null, null, testShip2, null, null, null, null]);
+        expect(testGameboard.board[6]).toStrictEqual([null, null, null, null, null, testShip2, null, null, null, null]);
+        expect(testGameboard.board[7]).toStrictEqual([null, null, null, null, null, testShip2, null, null, null, null]);
+    });
+
+    //should not allow occupation of a placed ship's cells or adjacent cells
 });
