d582cbfec8ccbf83c3c3ddd04853578df04e911c
andkymon <andkymon@gmail.com>
2025-02-15 14:20:02 +0800

    Configure eslint for webpack and jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,31 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import pluginJest from "eslint-plugin-jest";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    languageOptions: { globals: globals.browser },
-    env: {
-      browser: true,
-      amd: true,
-      node: true,
+    files: ["src/**/*.{js,jsx,ts,tsx}"], 
+    languageOptions: {
+      globals: 
+        globals.browser, 
+    },
+    ...pluginJs.configs.recommended,
+  },
+  {
+    files: ['**/*.spec.js', '**/*.test.js'],
+    plugins: {
+      jest: pluginJest,
+    },
+    languageOptions: {
+      globals: pluginJest.environments.globals.globals,
+    },
+    rules: {
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error',
     },
   },
-  pluginJs.configs.recommended,
 ];
