67d601e6bf75cf06278e79ae90bd9febb18b32a4
andkymon <andkymon@gmail.com>
2025-02-15 19:15:58 +0800

    Add and test Player class

---
+++ src/player.test.js
@@ -0,0 +1,7 @@
+import { Player } from "./player";
+import { Gameboard } from "./gameboard";
+
+test("should have ships property, an array of all ships", () => {
+    const testPlayer = new Player();
+    expect(testPlayer.gameboard).toStrictEqual(new Gameboard());
+});
