d7da119bcee4868ab4e0485cf223ccdc1a83e3c0
andkymon <andkymon@gmail.com>
2025-02-20 21:26:36 +0800

    Implement ship placement page
    - Added ship placement page logic for the human player
    - Implemented interactive grid that the player can click to place their
    ships
    - Updated game message to indicate current ship to be placed, also
    displays error messages when a desired ship placement is invalid.
    - Set up click, hover, and "r" click event listeners for styling and
    game logic updates
    - Applied styling for hovered and placed ships
    - Enabled ship rotation using the 'r' key

---
+++ src/shipPlacementPage.js
@@ -0,0 +1,140 @@
+export function startShipPlacement(human) {
+  // Used for game message display
+  const shipNames = [
+    "carrier",
+    "battleship",
+    "cruiser",
+    "submarine",
+    "destroyer",
+  ];
+
+  // Updated on successful ship placement
+  let currentShipIndex = 0;
+  let currentShip = human.gameboard.ships[currentShipIndex];
+
+  // Used for ship orientation logic
+  let isVertical = true;
+
+  // Display current ship to be placed
+  const gameMessage = document.querySelector("#game-message");
+  gameMessage.textContent = `Place your ${shipNames[currentShipIndex]}`;
+
+  // Generate grid squares
+  const grid = document.querySelector(".grid");
+  for (const [rowNumber, row] of human.gameboard.board.entries()) {
+    for (const colNumber of row.keys()) {
+      const gridSquare = document.createElement("button");
+      gridSquare.classList.add("grid-square");
+      gridSquare.setAttribute("data-row", rowNumber);
+      gridSquare.setAttribute("data-col", colNumber);
+      grid.appendChild(gridSquare);
+
+      // Each square's event listeners
+      gridSquare.addEventListener("click", () => {
+        const clickResult = human.gameboard.placeShip(
+          currentShip,
+          rowNumber,
+          colNumber,
+          isVertical,
+        );
+        if (clickResult !== undefined) {
+          gameMessage.textContent = clickResult;
+          return;
+        }
+        applyClickedStyling(rowNumber, colNumber);
+        updateCurrentShip();
+      });
+
+      gridSquare.addEventListener("mouseenter", () => {
+        applyHoveredStyling(rowNumber, colNumber);
+      });
+
+      gridSquare.addEventListener("mouseleave", () => {
+        clearHoveredStyling();
+      });
+    }
+  }
+
+  function applyClickedStyling(rowNumber, colNumber) {
+    // To set which axis to increment on iterations
+    const rowIncrement = isVertical ? 1 : 0;
+    const colIncrement = isVertical ? 0 : 1;
+
+    let clickedGridSquare;
+    let currentRow;
+    let currentCol;
+
+    for (let i = 0; i < currentShip.length; i++) {
+      currentRow = rowNumber + rowIncrement * i;
+      currentCol = colNumber + colIncrement * i;
+      clickedGridSquare = document.querySelector(
+        `[data-row="${currentRow}"][data-col="${currentCol}"]`,
+      );
+      clickedGridSquare.classList.add("clicked");
+    }
+
+    clearHoveredStyling();
+  }
+
+  function updateCurrentShip() {
+    currentShipIndex++;
+    gameMessage.textContent = `Place your ${shipNames[currentShipIndex]}`;
+    currentShip = human.gameboard.ships[currentShipIndex];
+  }
+
+  // Shared by style applying and clearing functions
+  let hoveredGridSquares = [];
+
+  // Used by "r" click event listener to determine which square is currently being hovered
+  let hoveredRowIndex;
+  let hoveredColIndex;
+
+  // Allow ship rotation when "r" key is pressed
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "r") {
+      isVertical = !isVertical;
+    }
+    clearHoveredStyling();
+    applyHoveredStyling(hoveredRowIndex, hoveredColIndex);
+  });
+
+  function applyHoveredStyling(rowNumber, colNumber) {
+    // Used by "r" click event listener to determine which square is currently being hovered
+    hoveredRowIndex = rowNumber;
+    hoveredColIndex = colNumber;
+
+    // To set which axis to increment on iterations
+    const rowIncrement = isVertical ? 1 : 0;
+    const colIncrement = isVertical ? 0 : 1;
+
+    let hoveredGridSquare;
+    let currentRow;
+    let currentCol;
+
+    for (let i = 0; i < currentShip.length; i++) {
+      currentRow = rowNumber + rowIncrement * i;
+      currentCol = colNumber + colIncrement * i;
+
+      // Avoid style application on non existent squares
+      if (currentRow > 9 || currentCol > 9) {
+        return;
+      }
+      hoveredGridSquare = document.querySelector(
+        `[data-row="${currentRow}"][data-col="${currentCol}"]`,
+      );
+      hoveredGridSquare.classList.add("hovered");
+      hoveredGridSquares.push(hoveredGridSquare);
+    }
+  }
+
+  function clearHoveredStyling() {
+    for (const hoveredGridSquare of hoveredGridSquares) {
+      hoveredGridSquare.classList.remove("hovered");
+    }
+    hoveredGridSquares = [];
+  }
+}
+
+//document event listener for allshipsplaced
+//dispatch event for next page
+//next page
