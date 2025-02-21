a7daa2e9030a64451f805d5b527a7dd65d82cf66
andkymon <andkymon@gmail.com>
2025-02-21 19:22:03 +0800

    Refactor and format game.js

--- src/game.js
+++ src/game.js
@@ -1,25 +1,23 @@
-
 import { Player } from "./logic/player.js";
 import { startShipPlacement } from "./shipPlacementPage.js";
 import { placeShipsRandomly } from "./placeComputerShips.js";
 import { startBattle } from "./battlePage.js";
 
 export function gameStart() {
-    const human = new Player();
-    const computer = new Player();
-    startShipPlacement(human);
+  const human = new Player();
+  const computer = new Player();
+  startShipPlacement(human);
 
-    document.addEventListener("allShipsPlaced", () => {
-        // Switch from ship placement page to battle page
-        const shipPlacementPage = document.querySelector("#ship-placement-page");
-        const battlePage = document.querySelector("#battle-page");
+  const handleAllShipsPlaced = () => {
+    // Switch from ship placement page to battle page
+    document.querySelector("#ship-placement-page").style.display = "none";
+    document.querySelector("#battle-page").style.display = "flex";
 
-        shipPlacementPage.style.display = "none";
-        battlePage.style.display = "flex";
+    placeShipsRandomly(computer);
+    startBattle(human, computer);
+  };
 
-        placeShipsRandomly(computer);
-        startBattle(human, computer);
-    });
-};
-
-
+  document.addEventListener("allShipsPlaced", handleAllShipsPlaced, {
+    once: true,
+  });
+}
