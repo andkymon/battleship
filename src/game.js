978d34a5cab302533a88fa016d370d45f2035039
andkymon <andkymon@gmail.com>
2025-02-21 10:21:41 +0800

    Add event listener to switch to  battle page

--- src/game.js
+++ src/game.js
@@ -6,6 +6,15 @@ export function gameStart() {
     const human = new Player();
     const computer = new Player();
     startShipPlacement(human);
+
+    document.addEventListener("allShipsPlaced", () => {
+        // Switch from ship placement page to battle page
+        const shipPlacementPage = document.querySelector("#ship-placement-page");
+        const battlePage = document.querySelector("#battle-page");
+
+        shipPlacementPage.style.display = "none";
+        battlePage.style.display = "flex";
+    });
 };
 
 
