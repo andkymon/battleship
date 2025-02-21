3b63fb895c73a1cdf809f0590ad316d5552842aa
andkymon <andkymon@gmail.com>
2025-02-21 19:14:47 +0800

    feat: implement the battle page
    - Added startBattle function to initialize the game with human and
    computer players.
    - Implemented generateGridSquares to create the game board for both
    players.
    - Created createHumanGridSquare and createComputerGridSquare functions
    to generate grid elements.
    - Added click event listener for computer grid squares to handle
    attacks.
    - Implemented attackComputer function to process human attacks on the
    computer's board.
    - Added applyHitStyling to visually indicate hits and misses.
    - Created computerTurn event listener to handle computer attacks.
    - Implemented selectRandomCoordinates for computer's random attack
    selection.
    - Created availableCoordinates object to track remaining attackable
    positions for computer.
    - Defined createCoordinatesObject to initialize an object containing all
    coordinates, for availableCoordinates object.
    - Added game messages to notify players of attack results and game
    outcomes.

--- src/battlePage.js
+++ src/battlePage.js
@@ -1,19 +1,144 @@
-export function startBattle(human, computer) {
-    
+let human;
+let computer;
+
+export function startBattle(player1, player2) {
+    human = player1;
+    computer = player2;
+
+    displayGameMessage("");
+    generateGridSquares();
 }
 
-    //func: populate computer squares efficiently
-            //if successful
-            //delete square on real obj (no need bc included in next)
-            //func: return adjacentsquareskeys[]
-            //for each 
-            //delete adjacent squares on real obj
+function displayGameMessage(string) {
+    const gameMessage = document.querySelector("#game-message");
+    gameMessage.textContent = string;
+}
 
-//adjacentsquares keys
-    //copy is spaceoccupied logic
-    //push ("", row, col)
+function generateGridSquares() {
+    const humanGrid = document.querySelector(".human.grid");
+    const computerGrid = document.querySelector(".computer.grid");
+
+    for (const [rowNumber, row] of human.gameboard.board.entries()) {
+        for (const colNumber of row.keys()) {
+            const humanGridSquare = createHumanGridSquare(rowNumber, colNumber);
+            humanGrid.appendChild(humanGridSquare);
+
+            const computerGridSquare = createComputerGridSquare(rowNumber, colNumber);
+            computerGrid.appendChild(computerGridSquare);
+        }
+    }
+}
+
+function createHumanGridSquare(rowNumber, colNumber) {
+    const humanGridSquare = document.createElement("div");
+    humanGridSquare.classList.add("human", "grid-square");
+    humanGridSquare.setAttribute("data-row", rowNumber);
+    humanGridSquare.setAttribute("data-col", colNumber);
+
+    if (human.gameboard.board[rowNumber][colNumber] !== null) {
+        humanGridSquare.classList.add("clicked");
+    }
+
+    return humanGridSquare;
+}
+
+function createComputerGridSquare(rowNumber, colNumber) {
+    const computerGridSquare = document.createElement("button");
+    computerGridSquare.classList.add("computer", "grid-square");
+    computerGridSquare.setAttribute("data-row", rowNumber);
+    computerGridSquare.setAttribute("data-col", colNumber);
+
+    computerGridSquare.addEventListener("click", () => {
+        attackComputer(computerGridSquare);
+    });
+    
+    return computerGridSquare;
+}
+
+function attackComputer(computerGridSquare) {
+    const row = computerGridSquare.getAttribute("data-row");
+    const column = computerGridSquare.getAttribute("data-col");
+    const attackResult = computer.gameboard.receiveAttack(row, column);
+
+    if (attackResult !== undefined) {
+        displayGameMessage(attackResult);
+        return;
+    }
+
+    displayGameMessage("");
+    applyHitStyling(computer, computerGridSquare);
+
+    // Check if human won
+    if (computer.gameboard.allShipsSunk() === true) {
+        displayGameMessage("Human wins.");
+        return;
+    }
+
+    const computerTurn = new Event("computerTurn");
+    document.dispatchEvent(computerTurn);
+}
+
+function applyHitStyling(player, gridSquare) {
+    // dataset property is an object containing an element's custom data attributes
+    const { row, column } = gridSquare.dataset;
+
+    // Blue on miss, red on hit
+    gridSquare.style.backgroundColor = player.gameboard.board[row][column] === "X" ? "blue" : "red";
+}
+
+document.addEventListener("computerTurn", () => {
+    // Disable human clicks
+    const computerGrid = document.querySelector(".computer.grid");
+    computerGrid.classList.add("disabled");
+
+    // Select random coordinates from selection pool, and attack
+    const randomCoordinates = selectRandomCoordinates();
+    const { row, column } = randomCoordinates;
+    human.gameboard.receiveAttack(row, column);
+
+    // Square will be blue on miss, red on hit
+    const humanGridSquare = document.querySelector(`.human.grid-square[data-row="${row}"][data-col="${column}"]`);
+    applyHitStyling(human, humanGridSquare);
+
+    // Check if computer won
+    if (human.gameboard.allShipsSunk() === true) {
+        displayGameMessage("Computer wins.");
+        return;
+    }
+
+    // If not, remove from selection pool so the coordinates do not get selected again
+    delete availableCoordinates[randomCoordinates.key];
+
+    // Re-enable human clicks for human's turn
+    computerGrid.classList.remove("disabled");
+});
+
+function selectRandomCoordinates() {
+    const keys = Object.keys(availableCoordinates);
+    const randomKey = keys[Math.floor(Math.random() * keys.length)];
+    availableCoordinates[randomKey].key = randomKey;
+    return availableCoordinates[randomKey];
+}
+
+const availableCoordinates = createCoordinatesObject();
+
+function createCoordinatesObject() {
+    // Object with index as key, and an object with row and column properties as the value
+    // Did not use array so their index stays the same even if other elements are deleted
+    const BOARD_SIZE = 10 * 10; // Board is a 10x10 grid.
+    const coordinates = {};
+  
+    for (let i = 0; i < BOARD_SIZE; i++) {
+      coordinates[i] = {};
+      coordinates[i] = { row: Math.floor(i / 10), col: i % 10 };
+    }
+  
+    return coordinates;
+}
 
 
-//generate human squares //divs
-//generate computer squares //buttons
-//battle logic
+
+
+
+
+
