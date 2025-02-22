91e91bd87dd001d1dd6b636796d3ef54474f9bd7
andkymon <andkymon@gmail.com>
2025-02-22 13:42:31 +0800

    Refactor: remove unnecessary abstractions

--- src/game.js
+++ src/game.js
@@ -3,6 +3,7 @@ import { startShipPlacement } from "./shipPlacementPage.js";
 import { placeShipsRandomly } from "./placeComputerShips.js";
 import { startBattle } from "./battlePage.js";
 
+// Declared here as it is used by event listeners
 let human;
 let computer;
 
@@ -10,45 +11,36 @@ export function gameStart() {
   human = new Player();
   computer = new Player();
 
-  displayShipPlacementPage();
-  startShipPlacement(human);
-}
-
-document.addEventListener("allShipsPlaced", handleAllShipsPlaced);
-document.addEventListener("gameEnd", handleGameEnd);
-
-const restartButton = document.querySelector("#restart");
-restartButton.addEventListener("click", gameStart);
-
-function displayShipPlacementPage() {
+  // Switch from end page to ship placement page
   document.querySelector("#ship-placement-page").style.display = "block";
-  document.querySelector("#battle-page").style.display = "none";
   document.querySelector("#win-page").style.display = "none";
   document.querySelector("#title").style.display = "block";
   document.querySelector("#game-message").style.display = "block";
+
+  // Allow human to place their ships
+  startShipPlacement(human);
 }
 
-function handleAllShipsPlaced() {
-  displayBattlePage();
-  placeShipsRandomly(computer);
-  startBattle(human, computer);
-}
-
-function handleGameEnd(event) {
-  displayEndPage();
-  document.querySelector("#win-page h1").textContent = event.detail;
-}
-
-function displayBattlePage() {
+document.addEventListener("allShipsPlaced", () => {
   // Switch from ship placement page to battle page
   document.querySelector("#ship-placement-page").style.display = "none";
   document.querySelector("#battle-page").style.display = "flex";
-}
 
-function displayEndPage() {
+  // Efficiently place a computer's ships randomly
+  placeShipsRandomly(computer);
+  startBattle(human, computer);
+});
+
+document.addEventListener("gameEnd", (event) => {
   // Switch from battle page to end page
   document.querySelector("#battle-page").style.display = "none";
   document.querySelector("#title").style.display = "none";
   document.querySelector("#game-message").style.display = "none";
   document.querySelector("#win-page").style.display = "flex";
-}
+
+  // Display who won
+  document.querySelector("#win-page h1").textContent = event.detail;
+});
+
+// Button to restart the game
+document.querySelector("button #restart").addEventListener("click", gameStart);
