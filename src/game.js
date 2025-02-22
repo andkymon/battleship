423c38fda25a7c3d9d848822d10c1052176af7d3
andkymon <andkymon@gmail.com>
2025-02-22 16:32:19 +0800

    Relocate placeShipsRandomly

--- src/game.js
+++ src/game.js
@@ -1,6 +1,6 @@
 import { Player } from "./logic/player.js";
 import { startShipPlacement } from "./shipPlacementPage.js";
-import { placeShipsRandomly } from "./placeShipsRandomly.js";
+import { placeShipsRandomly } from ".logic/placeShipsRandomly.js";
 import { startBattle } from "./battlePage.js";
 
 // Declared here as it is used by event listeners
