eee92ba03d1fd75014ce610d2f5bee114d33ff9a
andkymon <andkymon@gmail.com>
2025-02-23 15:43:09 +0800

    Add validation and error handling for argument

--- src/helpers/selectRandomCoordinates.js
+++ src/helpers/selectRandomCoordinates.js
@@ -1,6 +1,26 @@
 export function selectRandomCoordinates(availableCoordinates) {
+    checkIfObject(availableCoordinates);
+    checkIfObjectEmpty(availableCoordinates);
+
     const keys = Object.keys(availableCoordinates);
     const randomKey = keys[Math.floor(Math.random() * keys.length)];
     availableCoordinates[randomKey].key = randomKey;
     return availableCoordinates[randomKey];
+}
+
+function checkIfObject(availableCoordinates) {
+    if (
+        availableCoordinates === null ||
+        typeof availableCoordinates !== "object" ||
+        Array.isArray(availableCoordinates) || 
+        typeof availableCoordinates === "function"
+    ) {
+        throw new Error("Invalid input. selectRandomCoordinates expects an object.");
+    }
+}
+
+function checkIfObjectEmpty(availableCoordinates) {
+    if (Object.keys(availableCoordinates).length === 0) {
+        throw new Error("No available coordinates.");
+    }
 }
